a7bdeb9245487ad745dc9f653f624a5a686147ba
Aaron Glass <aaronaglass@yahoo.com>
2019-07-29 23:58:03 -0400

    test

--- seleniumPython/features/features/example.py
+++ seleniumPython/features/features/example.py
@@ -5,39 +5,38 @@ import time
 
 from support.browser import Browsers
 
-
 class example_page():
 
-    def populate_name(context):
+    def enter_and_validate_text_input_xpath(context):
+        # Scenario: Enter and validate text input
+        # Given I am on the TestCafe example homepage
+        # When enter a name for 'Your Name'
+        # Then name entered displays in the input box
         context.browser = Browsers()
         driver = context.browser.browser_select('firefox')
+        driver.find_element(By.XPATH, "//input[@id='developer-name']").send_keys("John Smith")
+        verifyText = driver.find_element(By.XPATH, "//input[@id='developer-name']")
+        elementText = verifyText.get_attribute("value")
+        print("Text on element is " + elementText)
+        assert_that(elementText, equal_to("John Smith"))
+        time.sleep(2)
+        driver.quit()
 
-        driver.find_element(By.XPATH, "//input[@id='developer-name']").send_keys("first")
-        driver.find_element(By.ID, "developer-name").send_keys(" test")
-        driver.find_element(By.ID, "reusing-js-code").click()
-        driver.find_element(By.XPATH, "//input[@id='continuous-integration-embedding']").click()
-
+    def enter_and_validate_text_input(context):
+        # Scenario: Enter and validate text input
+        # Given I am on the TestCafe example homepage
+        # When enter a name for 'Your Name'
+        # Then name entered displays in the input box
+        context.browser = Browsers()
+        driver = context.browser.browser_select('firefox')
+        driver.find_element(By.ID, "developer-name").send_keys("Mike Smith")
         verifyText = driver.find_element(By.ID, "developer-name")
         elementText = verifyText.get_attribute("value")
-        print("Text on element is" + elementText)
-
-        assert_that(elementText, equal_to("first test"))
-
+        print("Text on element is " + elementText)
+        assert_that(elementText, equal_to("Mike Smith"))
         time.sleep(2)
         driver.quit()
-
-    def check_box(context):
-        context.browser = Browsers()
-        driver = context.browser.browser_select('firefox')
-        driver.find_element(By.ID, "reusing-js-code").click()
-        driver.find_element(By.XPATH, "//input[@id='continuous-integration-embedding']").click()
-
-        print("Second Text on element is")
-
-        time.sleep(2)
-        driver.quit()
-
 
 run_test = example_page()
-run_test.populate_name()
-run_test.check_box()
+run_test.enter_and_validate_text_input_xpath()
+run_test.enter_and_validate_text_input()
