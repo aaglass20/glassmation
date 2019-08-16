b2acdc2960c2712902eb4155fe962c5972b4dfde
Aaron Glass <aaronaglass@yahoo.com>
2019-08-15 22:19:02 -0400

    stuff

--- seleniumPython/features/features/example.py
+++ seleniumPython/features/features/example.py
@@ -5,6 +5,7 @@ import time
 
 from support.browser import Browsers
 
+
 class example_page():
 
     def enter_and_validate_text_input_xpath(context):
@@ -37,6 +38,28 @@ class example_page():
         time.sleep(2)
         driver.quit()
 
+    def check_box(context):
+        context.browser = Browsers()
+        driver = context.browser.browser_select('firefox')
+        driver.find_element(By.ID, "reusing-js-code").click()
+        driver.find_element(By.XPATH, "//input[@id='continuous-integration-embedding']").click()
+        isCheckedJS = driver.find_element_by_id("reusing-js-code").get_attribute("checked")
+        isCheckedCIE = driver.find_element_by_id("continuous-integration-embedding").get_attribute("checked")
+        assert_that(isCheckedJS, equal_to('true'))
+        assert_that(isCheckedCIE, equal_to('true'))
+
+
+        if isCheckedJS is not None:
+            print("Element checked - ", isCheckedJS)
+        else:
+            print("Element checked - false")
+
+
+        time.sleep(2)
+        driver.quit()
+
+
 run_test = example_page()
 run_test.enter_and_validate_text_input_xpath()
 run_test.enter_and_validate_text_input()
+run_test.check_box()
