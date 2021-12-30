4f6526ad19624563a243d505ee383202450f290e
Aaron Glass <aaronglass@Aarons-MacBook-Pro.local>
2021-12-29 21:15:37 -0500

    update api, find by name

--- Cypress/cypress/api/mlb.js
+++ Cypress/cypress/api/mlb.js
@@ -1,15 +1,16 @@
 export class MLBAPI {
 
-    getPlayerName() {
+    getPlayerName(searchName, fullName) {
         cy.request({
             method: 'GET',
-            url: Cypress.env('MLB_BASE_URL') + '/json/named.search_player_all.bam?active_sw=%27Y%27&name_part=%27Lindor%25%27',
+            url: `${Cypress.env('MLB_BASE_URL')}/json/named.search_player_all.bam?active_sw=%27Y%27&name_part=%27${searchName}%25%27`,
             headers: {},
             form: true,
             body: {}
         }).then((response) => {
             expect(response.status).to.eq(200)
             let results = JSON.parse(JSON.stringify(response.body)).search_player_all.queryResults.row
+            expect(results.name_display_first_last).to.include(fullName)
             cy.log(JSON.parse(JSON.stringify(response.body)).search_player_all.queryResults.row.team_full)
             cy.log(results.team_full)
             cy.log(results.name_display_first_last)
@@ -19,37 +20,50 @@ export class MLBAPI {
             cy.log(JSON.stringify(response.body))
             cy.wrap(name).as('name')
             cy.wrap(team).as('team')
-            cy.wrap(playerId).as('playId')
-            beforeEach(function () {
-                cy.log('Global Variable')
-                cy.wrap(name).as('name')
-                cy.wrap(team).as('team')
-                cy.wrap(playerId).as('playerId')
-            })
+            cy.wrap(playerId).as('playerId')
         })
     }
 
     teamsPlayerHasPlayedOn() {
-
         cy.get('@playerId').then(playerId => {
             cy.request({
                 method: 'GET',
-                url: Cypress.env('MLB_BASE_URL') + '/json/named.player_teams.bam?season=\'2014\'&player_id=' + playerId,
+                url: `${Cypress.env('MLB_BASE_URL')}/json/named.player_teams.bam?season=\'2014\'&player_id=${playerId}`,
                 headers: {},
                 form: true,
                 body: {}
             }).then((response) => {
                 expect(response.status).to.eq(200)
-                // let results = JSON.parse(JSON.stringify(response.body)).search_player_all.queryResults.row
-                // cy.log(JSON.parse(JSON.stringify(response.body)).search_player_all.queryResults.row.team_full)
-                // cy.log(results.team_full)
-                // cy.log(results.name_display_first_last)
-                // let name = results.name_display_first_last
-                // let team = results.team_full
-                // let playerId = results.player_id
                 cy.log(JSON.stringify(response.body))
-
             })
         })
     }
+
+
+    getByPlayerName40Roster(team_id, name) {
+        cy.request({
+            method: 'GET',
+            url: `${Cypress.env('MLB_BASE_URL')}//json/named.roster_40.bam?team_id=${team_id}`,
+            headers: {},
+            form: true,
+            body: {}
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            const results = response.body.roster_40.queryResults.row
+            const matchedName = results.find((playerInfo) => {
+                return playerInfo.name_last === name;
+            });
+            cy.wrap(matchedName).as("playerInfo");
+        })
+    }
+
+    validatePlayerInfo(playerData) {
+        cy.get("@playerInfo").then((playerInfo) => {
+            expect(playerInfo.position_txt).to.eq(playerData[0])
+            expect(playerInfo.team_name).to.eq(playerData[1])
+            expect(playerInfo.name_display_first_last).to.eq(playerData[2])
+            expect(playerInfo.jersey_number).to.eq(playerData[3])
+        })
+    }
+
 }
--- Cypress/cypress/integration/testing/cleAPI.js
+++ Cypress/cypress/integration/testing/cleAPI.js
@@ -1,15 +1,22 @@
 import {MLBAPI} from "../../api/mlb";
 
 const mlbAPI = new MLBAPI()
+const playerInfo = ['P', 'Cleveland Indians', 'Shane Bieber', '57']
 
 describe('MLB API Test', function () {
 
     it('By Player Name', function () {
-        mlbAPI.getPlayerName()
+        mlbAPI.getPlayerName('Bieber', 'Shane Bieber')
     })
 
     it('Teams Player has Played On', function () {
+        mlbAPI.getPlayerName('Lindor', 'Francisco Lindor')
         mlbAPI.teamsPlayerHasPlayedOn()
     })
-    
+
+    it('Search Guardians 40 Man Roster by Player Name and Validate Player Info', function () {
+        mlbAPI.getByPlayerName40Roster(114, 'Bieber')
+        mlbAPI.validatePlayerInfo(playerInfo)
+    })
+
 })
